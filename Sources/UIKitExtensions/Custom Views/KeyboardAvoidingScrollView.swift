2a03aad67db8042b8d6ee0fad8ff3b7cb67c4564
GenericUser8 <unwound.podium_0y@icloud.com>
2025-11-10 21:56:59 +1100

    Added keyboard avoiding using notifications This is compile-guarded, as the keyboard-avoiding API is not available on tvOS

--- Sources/UIKitExtensions/Custom Views/KeyboardAvoidingScrollView.swift
+++ Sources/UIKitExtensions/Custom Views/KeyboardAvoidingScrollView.swift
@@ -46,9 +46,89 @@ open class KeyboardAvoidingScrollView: UIScrollView {
     }
     
     
+    // MARK: - Keyboard Avoidance
+
+#if !os(tvOS)
+    
+    @available(tvOS, unavailable)
+    @objc(UIEKeyboardAvoidingScrollViewKeyboardBehaviour)
+    public enum KeyboardBehaviour: UInt8 {
+        case notificationBased
+//        case constraintBased
+    }
+
+    @objc
+    @available(tvOS, unavailable)
+    open var preferredKeyboardBehaviour: KeyboardBehaviour = .notificationBased {
+        didSet {
+            updatePreferredKeyboardBehaviour()
+        }
+    }
+
+    @available(tvOS, unavailable)
+    private func updatePreferredKeyboardBehaviour() {
+        let center = NotificationCenter.default
+        switch preferredKeyboardBehaviour {
+        case .notificationBased:
+            if !registeredForKeyboardNotifications {
+                center.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+                center.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+                registeredForKeyboardNotifications = true
+            }
+//        case .constraintBased:
+//            guard #available(iOS 15.0, tvOS 15.0, visionOS 1.0, *) else { return }
+//            if registeredForKeyboardNotifications {
+//                center.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+//                center.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+//                registeredForKeyboardNotifications = false
+//            }
+        }
+    }
+    
+    private var registeredForKeyboardNotifications: Bool = false
+    
+    private var contentHeightOffset: CGFloat = 0 {
+        didSet {
+            self.contentInset.bottom += (contentHeightOffset - oldValue)
+        }
+    }
+    
+    @available(tvOS, unavailable)
+    @objc private func keyboardWillShow(_ notification: Notification) {
+        let screen = notification.object as? UIScreen
+        guard let userInfo = notification.userInfo,
+              let keyboardFrameEnd = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect else { return }
+        
+        let convertedKeyboardFrameEnd: CGRect
+        if let screen = screen {
+            convertedKeyboardFrameEnd = screen.coordinateSpace.convert(keyboardFrameEnd, to: self)
+        } else {
+            convertedKeyboardFrameEnd = keyboardFrameEnd
+        }
+        
+        let viewIntersection = self.bounds.intersection(convertedKeyboardFrameEnd)
+        guard !viewIntersection.isEmpty else { return }
+        
+        contentHeightOffset = self.bounds.maxY - viewIntersection.minY
+    }
+    
+    @objc private func keyboardWillHide(_ notification: Notification) {
+        contentHeightOffset = 0
+    }
+    
+    private func configureKeyboardAvoidance() {
+        updatePreferredKeyboardBehaviour()
+    }
+    
+#endif // !os(tvOS)
+
+    
     // MARK: - Configuration
     
-    public convenience init(frame: CGRect = .zero, scrollBehavior: ScrollBehavior) {
+    public convenience init(
+        frame: CGRect = .zero,
+        scrollBehavior: ScrollBehavior = .all
+    ) {
         self.init(frame: frame)
         self.scrollBehavior = scrollBehavior
     }
@@ -65,6 +145,9 @@ open class KeyboardAvoidingScrollView: UIScrollView {
     
     private func configure() {
         configureScrollBehaviour()
+#if !os(tvOS)
+        configureKeyboardAvoidance()
+#endif
     }
     
 }
@@ -74,7 +157,7 @@ open class KeyboardAvoidingScrollView: UIScrollView {
 
 import SwiftUI
 
-@available(iOS 17.0, *)
+@available(iOS 17.0, tvOS 17.0, *)
 #Preview {
     let view = KeyboardAvoidingScrollView()
     return view
